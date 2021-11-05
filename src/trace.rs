fd77cd1a6d104042c63fdde7b01cea05a91972e8
Travis Alan Baumbaugh <travis.baumbaugh@toposware.com>
2021-11-04 23:29:23 -0400

    Reverse the order of trace construction
    To complete reversing the order of the main components, the trace is
    now constructed with the main components (Schnorr and Merkle) swapped.
    This leaves rearrangement of the minor components and copy columns.

--- src/trace.rs
+++ src/trace.rs
@@ -18,6 +18,7 @@ use winterfell::{
 #[cfg(feature = "concurrent")]
 use winterfell::iterators::*;
 
+use merkle_const::PREV_TREE_ROOT_POS;
 use merkle_const::TRANSACTION_CYCLE_LENGTH as MERKLE_UPDATE_LENGTH;
 use schnorr_const::SIG_CYCLE_LENGTH as SCHNORR_LENGTH;
 
@@ -82,19 +83,23 @@ pub fn build_trace(tx_metadata: &TransactionMetadata) -> ExecutionTrace<BaseElem
                         s_old_values[i],
                         r_old_values[i],
                         deltas[i],
+                        signatures[i],
                         state,
                     );
                 },
                 |step, state| {
                     update_transaction_state(
                         step,
+                        initial_roots[i],
                         s_indices[i],
                         r_indices[i],
+                        s_old_values[i],
+                        r_old_values[i],
                         s_paths[i].clone(),
                         r_paths[i].clone(),
+                        deltas[i],
                         delta_bits,
                         sigma_bits,
-                        signatures[i],
                         sig_bits,
                         sig_hash_bits,
                         message,
@@ -116,16 +121,21 @@ pub fn init_transaction_state(
     s_old_value: [BaseElement; 4],
     r_old_value: [BaseElement; 4],
     delta: BaseElement,
+    signature: (BaseElement, Scalar),
     state: &mut [BaseElement],
 ) {
-    // Initialize leaf values prior to hashing
-    merkle::update::init_merkle_update_state(
-        initial_root,
-        s_old_value,
-        r_old_value,
-        delta,
-        &mut state[..merkle_const::TRACE_WIDTH],
-    );
+    schnorr::init_sig_verification_state(signature, &mut state[..schnorr_const::TRACE_WIDTH]);
+    // We set the 4 registers next to the Schnorr signature sub-trace to zero, for computing
+    // the range proofs on delta and sigma = sender_balance - delta
+    let start_range_index = schnorr_const::TRACE_WIDTH;
+    range::init_range_verification_state(&mut state[start_range_index..start_range_index + 2]);
+    range::init_range_verification_state(&mut state[start_range_index + 2..start_range_index + 4]);
+    state[RX_COPY_POS] = signature.0;
+
+    // Set the initial root
+    let init_root = initial_root.to_elements();
+    state[PREV_TREE_ROOT_POS] = init_root[0];
+    state[PREV_TREE_ROOT_POS + 1] = init_root[1];
 
     // Copy public keys and sigma = balance_sender - delta
     let start_copy_index = merkle_const::TRACE_WIDTH;
@@ -144,47 +154,30 @@ pub fn init_transaction_state(
 #[allow(clippy::too_many_arguments)]
 pub fn update_transaction_state(
     step: usize,
+    initial_root: rescue::Hash,
     s_index: usize,
     r_index: usize,
+    s_old_value: [BaseElement; 4],
+    r_old_value: [BaseElement; 4],
     s_branch: Vec<rescue::Hash>,
     r_branch: Vec<rescue::Hash>,
+    delta: BaseElement,
     delta_bits: &BitSlice<Lsb0, u8>,
     sigma_bits: &BitSlice<Lsb0, u8>,
-    signature: (BaseElement, Scalar),
     sig_bits: &BitSlice<Lsb0, u8>,
     sig_hash_bits: &BitSlice<Lsb0, u8>,
     message: [BaseElement; 6],
     pkey_point: [BaseElement; 3],
     state: &mut [BaseElement],
 ) {
-    let merkle_update_flag = step < MERKLE_UPDATE_LENGTH - 1;
-    let schnorr_init_flag = step == MERKLE_UPDATE_LENGTH - 1;
-    let schnorr_update_flag = !schnorr_init_flag && (step < SCHNORR_LENGTH + MERKLE_UPDATE_LENGTH);
+    let schnorr_update_flag = step < SCHNORR_LENGTH - 1;
+    let merkle_init_flag = step == SCHNORR_LENGTH - 1;
+    let merkle_update_flag = !merkle_init_flag && step < SCHNORR_LENGTH + MERKLE_UPDATE_LENGTH;
 
-    if merkle_update_flag {
-        // Proceed to Merkle authentication paths verification
-        merkle::update::update_merkle_update_state(
-            step,
-            s_index,
-            r_index,
-            s_branch,
-            r_branch,
-            &mut state[..merkle_const::TRACE_WIDTH],
-        );
     // Initialize Schnorr signature verification state
-    } else if schnorr_init_flag {
-        schnorr::init_sig_verification_state(signature, &mut state[..schnorr_const::TRACE_WIDTH]);
-        // We set the 4 registers next to the Schnorr signature sub-trace to zero, for computing
-        // the range proofs on delta and sigma = sender_balance - delta
-        let start_range_index = schnorr_const::TRACE_WIDTH;
-        range::init_range_verification_state(&mut state[start_range_index..start_range_index + 2]);
-        range::init_range_verification_state(
-            &mut state[start_range_index + 2..start_range_index + 4],
-        );
-        state[RX_COPY_POS] = signature.0;
-    } else if schnorr_update_flag {
+    if schnorr_update_flag {
         // Proceed to Schnorr signature verification
-        let schnorr_step = step - MERKLE_UPDATE_LENGTH;
+        let schnorr_step = step;
         schnorr::update_sig_verification_state(
             schnorr_step,
             message,
@@ -221,5 +214,25 @@ pub fn update_transaction_state(
                 state[SIGMA_COPY_POS], state[SIGMA_ACCUMULATE_POS],
             );
         }
+    } else if merkle_init_flag {
+        // Initialize leaf values prior to hashing
+        merkle::update::init_merkle_update_state(
+            initial_root,
+            s_old_value,
+            r_old_value,
+            delta,
+            &mut state[..merkle_const::TRACE_WIDTH],
+        );
+    } else if merkle_update_flag {
+        let merkle_step = step - SCHNORR_LENGTH;
+        // Proceed to Merkle authentication paths verification
+        merkle::update::update_merkle_update_state(
+            merkle_step,
+            s_index,
+            r_index,
+            s_branch,
+            r_branch,
+            &mut state[..merkle_const::TRACE_WIDTH],
+        );
     }
 }
